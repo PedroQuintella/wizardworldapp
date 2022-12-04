e92fec0da8e278c82fc09f8aecabe14303ee1143
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-12-04 18:22:58 -0300

    Feat: Models House e Spell
    Cria os models House e Spell para definir os atributos das casas e das magias do aplicativo.

---
+++ app/src/main/java/com/app/wizardworld/model/Spell.java
@@ -0,0 +1,70 @@
+package com.app.wizardworld.model;
+
+public class Spell {
+    private String id;
+    private String name;
+    private String incantation;
+    private String effect;
+    private String type;
+    private String light;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIncantation() {
+        return incantation;
+    }
+
+    public void setIncantation(String incantation) {
+        this.incantation = incantation;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+
+    public void setEffect(String effect) {
+        this.effect = effect;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLight() {
+        return light;
+    }
+
+    public void setLight(String light) {
+        this.light = light;
+    }
+
+    @Override
+    public String toString() {
+        return "Spell{" +
+                "name='" + name + '\'' +
+                ", incantation='" + incantation + '\'' +
+                ", effect='" + effect + '\'' +
+                ", type='" + type + '\'' +
+                ", light='" + light + '\'' +
+                '}';
+    }
+}
